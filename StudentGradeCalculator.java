04926bac2fe31b1642f923196a8ab5125eb22804
Anushka-dimri <anushkadimri02@gmail.com>
2025-02-04 17:09:23 +0530

    Add files via upload

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+        int[] marks = new int[numSubjects];
+        System.out.println("Enter the marks obtained in each subject (out of 100):");
+        int totalMarks = 0;
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+            totalMarks += marks[i];
+        }
+        double averagePercentage = (double) totalMarks / numSubjects;
+        String grade;
+        if (averagePercentage >= 90) {
+            grade = "A+";
+        } else if (averagePercentage >= 80) {
+            grade = "A";
+        } else if (averagePercentage >= 70) {
+            grade = "B+";
+        } else if (averagePercentage >= 60) {
+            grade = "B";
+        } else if (averagePercentage >= 50) {
+            grade = "C";
+        } else if (averagePercentage >= 40) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+        System.out.println("\nTotal Marks: " + totalMarks);
+        System.out.printf("Average Percentage: %.2f%%\n", averagePercentage);
+        System.out.println("Grade: " + grade);
+        scanner.close();
+    }
+}
